2084090986719f1e5e722287e7f1e2cae559a141
Ryan Nelson <rnelson91295@gmail.com>
2022-08-23 20:06:27 -0400

    Set up build tools and npm dependencies

---
+++ client/src/index.jsx
@@ -0,0 +1,41 @@
+// Bring React in to build a component.
+import React from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  }
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  }
+});
+
+// Huzzah for jsx!
+const App = () => {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      <h1>Hello World!!!!!!!</h1>
+    </ThemeProvider>
+  );
+}
+
+root.render(<App />);
