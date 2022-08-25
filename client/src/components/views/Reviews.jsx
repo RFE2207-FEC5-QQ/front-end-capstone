a3bbf4f00349ec88af853c7e6909fbddb8e3c5b5
Ryan Nelson <rnelson91295@gmail.com>
2022-08-25 19:10:55 -0400

    Reviews component GET request to server

--- client/src/components/views/Reviews.jsx
+++ client/src/components/views/Reviews.jsx
@@ -1,10 +1,34 @@
 import React from 'react'; // React module is imported if you choose to convert to class component, remove the import if not
+import axios from 'axios';
 
-const Reviews = () => {
-  return (
-    <div className='view-reviews'>
-    </div>
-  );
-};
+class Reviews extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      productId: 1,
+      reviews: []
+    };
+  }
+
+  getReviews() {
+    axios.get('/reviews', {params: {'product_id': this.state.productId}})
+      .then((success) => {
+        this.setState({reviews: success.data});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    console.log('reviews', reviews);
+    return (
+      <div className='view-reviews'>
+      </div>
+    );
+  }
+
+}
 
 export default Reviews;
