a7c3405ec53f6bcd31b3e50ab4a998fa6f4068ad
Ryan Nelson <rnelson91295@gmail.com>
2022-08-26 19:10:32 -0400

    Added route to server for reviews/meta

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -28,6 +28,28 @@ router.get('/reviews', (req, res, next) => {
     });
 });
 
+router.get('/reviews/meta', (req, res, next) => {
+  if (!req.query.productId) {
+    res.sendStatus(404);
+    return;
+  }
+  axios.get(process.env.API_URL + 'reviews/meta',
+    {
+      params: {
+        'product_id': req.query.productId
+      },
+      headers: { 'Authorization': process.env.GITHUB_AUTH }
+    }
+  )
+    .then((success) => {
+      res.status(200).send(success.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(404);
+    });
+});
+
 router.post('/reviews', (req, res, next) => {
   axios.post(process.env.API_URL + 'reviews/',
     {
