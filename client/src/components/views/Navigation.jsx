01cc3f850833fac4058501afb6a8a76b8fb491a8
ytang <107004276+yota88@users.noreply.github.com>
2022-08-26 13:47:43 -0400

    Created template for navbar.

--- client/src/components/views/Navigation.jsx
+++ client/src/components/views/Navigation.jsx
@@ -1,9 +1,46 @@
 import React from 'react'; // React module is imported if you choose to convert to class component, remove the import if not
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+// import '../../fonts/ElanRegular.ttf';
 
 const Navigation = () => {
+
   return (
-    <div className='view-navigation'>
-    </div>
+    <Box className='view-navigation' sx={{ flexGrow: 1 }}>
+      <AppBar
+        position='sticky'
+        elevation={0}
+        sx={{backgroundColor: 'white', color: 'black'}}>
+        <Typography
+          className='main-header'
+          variant='h2'
+          component='div'
+          sx={{ flexGrow: 1, padding: 2, margin: 'auto'}}>
+          ATELIER
+        </Typography>
+        <Divider variant='middle'/>
+        <Typography
+          variant='h6'
+          component='div'
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            padding: 1,
+          }}>
+          <div>PRODUCT DETAIL</div>
+          <div>RELATED PRODUCTS</div>
+          <div>QUESTIONS</div>
+          <div>REVIEWS</div>
+        </Typography>
+        <Divider variant='middle'/>
+      </AppBar>
+    </Box>
   );
 };
 
