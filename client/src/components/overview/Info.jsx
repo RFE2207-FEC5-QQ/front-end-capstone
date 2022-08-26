81d8eef2457dc3948a4a4362f5088b6e1cd5fe41
Daniel Shin <daniel990402@gmail.com>
2022-08-26 19:36:04 -0400

    added rating

--- client/src/components/overview/Info.jsx
+++ client/src/components/overview/Info.jsx
@@ -1,14 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Box, Rating } from '@mui/material'
 
 const Info = ({product}) => {
+
+  const [rating, setRating] = useState(0)
+
+  const getAvgRating = (ratings) => {
+    var total = 0;
+    var count = 0;
+    for (var rating in ratings) {
+      total += (rating * Number(ratings[rating]));
+      count += Number(ratings[rating]);
+    }
+    var average = Math.ceil((total / count) * 100) / 100;
+    setRating(average);
+  }
+
+  const getRatings = (id) => {
+    var options = {
+      method: 'get',
+      url: '/reviews/meta',
+      params: {
+        productId: product.id
+      }
+    }
+    axios(options)
+      .then(result => {
+        getAvgRating(result.data.ratings);
+      })
+  }
+
+  useEffect(() => {
+    getRatings();
+  })
 
   return (
     <div className='product-info'>
       <div className='product-rating'>
         <Rating
           name='rating'
-          value={2.5}
+          value={rating}
           precision={0.25}
           readOnly
         />
