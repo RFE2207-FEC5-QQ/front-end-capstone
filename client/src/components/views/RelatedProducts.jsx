074fb13859e5f4fe01bce5da460ee856bdd8af6e
ytang <107004276+yota88@users.noreply.github.com>
2022-08-27 10:26:57 -0400

    Modified /related route to call Atelier API. Modified client to request related products on mount.

--- client/src/components/views/RelatedProducts.jsx
+++ client/src/components/views/RelatedProducts.jsx
@@ -7,58 +7,55 @@ import { Paper, Button, Box } from '@mui/material';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+
 const RelatedProducts = () => {
   // Use placeholder productId passed down from global state for now.
   // const [related, setRelated] = useState(null);
-  const [related, setRelated] = useState([
-    37312,
-    37313,
-    37318,
-    37317
-  ]);
+  const [relatedList, setRelatedList] = useState([]);
 
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 4
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
 
-  // useEffect(() => {
-  //   axios
-  //     .get('/related', {
-  //       params: {
-  //         // productId
-  //         productId: 37311,
-  //       }
-  //     })
-  //     .then((results) => {
-  //       setRelated(results);
-  //     })
-  //     .catch((err) => {
-  //       throw ('Error fetching related products');
-  //     });
-  // // }, [productId]);
-  // }, [1]);
+  useEffect(() => {
+    axios
+      .get('/related', {
+        params: {
+          productId: 37311,
+        }
+      })
+      .then((results) => {
+        const relatedIds = results.data;
+        console.log('relatedIds', relatedIds);
+        setRelatedList(relatedIds);
+        return;
+      })
+      .catch((err) => {
+        throw ('Error fetching related products');
+      });
+  }, []);
 
-  let relatedMap = related.map((product, i) => <RelatedCard key={i} item={product}/>);
+  let relatedCards = relatedList.map((product, i) => <RelatedCard key={i} item={product}/>);
 
   return (
     <Carousel className='carousel' responsive={responsive}>
-      {relatedMap}
+      {relatedCards}
     </Carousel>
   );
 };
--- server/routes/related.js
+++ server/routes/related.js
@@ -4,7 +4,21 @@ const axios = require('axios');
 const router = express.Router();
 
 router.get('/related', (req, res, next) => {
-
+  const productId = req.query.productId;
+  const relatedURL = process.env.API_URL + `products/${productId}/related`;
+  axios
+    .get(relatedURL, {
+      headers: {
+        Authorization: process.env.GITHUB_AUTH,
+      }
+    })
+    .then((results) => {
+      const relatedIds = results.data;
+      res.status(200).send(relatedIds);
+    })
+    .catch((err) => {
+      throw ('Error calling Atelier API!');
+    });
 });
 
 router.post('/related', (req, res, next) => {
