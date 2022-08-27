678f122e6bf2f45344169a872e1d7d1b6380841f
Daniel Shin <daniel990402@gmail.com>
2022-08-27 18:44:12 -0400

    implemented sale prices

--- client/src/components/overview/Info.jsx
+++ client/src/components/overview/Info.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Box, Rating } from '@mui/material'
 
-const Info = ({product}) => {
+const Info = ({ product, selectedStyle }) => {
 
   const [rating, setRating] = useState(0)
 
@@ -29,39 +29,74 @@ const Info = ({product}) => {
       .then(result => {
         getAvgRating(result.data.ratings);
       })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   useEffect(() => {
     getRatings();
   })
-
-  return (
-    <div className='product-info'>
-      <div className='product-rating'>
-        <Rating
-          name='rating'
-          value={rating}
-          precision={0.25}
-          readOnly
-        />
-        Read All Reviews
+  if (selectedStyle.sale_price) {
+    return (
+      <div className='product-info'>
+        <div className='product-rating'>
+          <Rating
+            name='rating'
+            value={rating}
+            precision={0.25}
+            readOnly
+          />
+          Read All Reviews
+        </div>
+        <div className='product-category'>
+          {product.category}
+        </div>
+        <div className='product-name'>
+          {product.name}
+        </div>
+        <div className='original-price'>
+          {`$${selectedStyle.original_price}`}
+        </div>
+        <div className='sale-price'>
+          {`$${selectedStyle.sale_price}`}
+        </div>
+        <div className='product-social-media'>
+          Facebook
+          Twitter
+          Pinterest
+        </div>
       </div>
-      <Box>
-        {product.category}
-      </Box>
-      <div className='product-name'>
-        {product.name}
+    )
+  } else {
+    return (
+      <div className='product-info'>
+        <div className='product-rating'>
+          <Rating
+            name='rating'
+            value={rating}
+            precision={0.25}
+            readOnly
+          />
+          Read All Reviews
+        </div>
+        <div className='product-category'>
+          {product.category}
+        </div>
+        <div className='product-name'>
+          {product.name}
+        </div>
+        <div className='product-price'>
+          {`$${selectedStyle.original_price}`}
+        </div>
+        <div className='product-social-media'>
+          Facebook
+          Twitter
+          Pinterest
+        </div>
       </div>
-      <div className='product-price'>
-        {`$${product.default_price}`}
-      </div>
-      <div className='product-social-media'>
-        Facebook
-        Twitter
-        Pinterest
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Info;
