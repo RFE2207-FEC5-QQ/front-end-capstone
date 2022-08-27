0d586841f9c98c601d16a8cd933dd13ce4fe9827
ytang <107004276+yota88@users.noreply.github.com>
2022-08-27 08:40:26 -0400

    Created base template for related product carousel and cards.

--- client/src/components/views/RelatedProducts.jsx
+++ client/src/components/views/RelatedProducts.jsx
@@ -2,43 +2,62 @@ import React, { useState, useEffect } from 'react'; // React module is imported 
 import axios from 'axios';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import RelatedCard from './RelatedCard.jsx';
-import Carousel from 'react-material-ui-carousel';
-import { Paper, Button } from '@mui/material';
+// import Carousel from 'react-material-ui-carousel';
+import { Paper, Button, Box } from '@mui/material';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const RelatedProducts = () => {
   // Use placeholder productId passed down from global state for now.
-  const [related, setRelated] = useState(null);
+  // const [related, setRelated] = useState(null);
+  const [related, setRelated] = useState([
+    37312,
+    37313,
+    37318,
+    37317
+  ]);
 
-  useEffect(() => {
-    axios
-      .get('/related', {
-        params: {
-          // productId
-          productId: 1,
-        }
-      })
-      .then((results) => {
-        setRelated(results);
-      })
-      .catch((err) => {
-        throw ('Error fetching related products');
-      });
-  // }, [productId]);
-  }, [1]);
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
-
-
-  // return (
-  //   <div className='view-related'>
-  //   </div>
-  // );
+  // useEffect(() => {
+  //   axios
+  //     .get('/related', {
+  //       params: {
+  //         // productId
+  //         productId: 1,
+  //       }
+  //     })
+  //     .then((results) => {
+  //       setRelated(results);
+  //     })
+  //     .catch((err) => {
+  //       throw ('Error fetching related products');
+  //     });
+  // // }, [productId]);
+  // }, [1]);
+  let relatedMap = related.map((product, i) => <RelatedCard key={i} item={product}/>);
 
   return (
-    <Carousel>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
+    <Carousel className='carousel' responsive={responsive}>
+      {relatedMap}
     </Carousel>
   );
 };
