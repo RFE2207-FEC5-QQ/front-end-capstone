e4f2fc7ff4b5f3ac1622b5678feb86650e4cd085
ytang <107004276+yota88@users.noreply.github.com>
2022-08-27 11:41:15 -0400

    Added temporary route for /styles. Will delete later. Updated relatedcard to be able to render actual product image from API.

--- client/src/components/cards/RelatedCard.jsx
+++ client/src/components/cards/RelatedCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
   Box,
   CardMedia,
@@ -12,7 +13,9 @@ import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 
 const RelatedCard = ({ item }) => {
 
-  const [imageURL, setimageURL] = useState('');
+  // const [imageURL, setimageURL] = useState('');
+  const [style, setStyle] = useState(null);
+  const [imgURL, setImgURL] = useState(null);
 
   useEffect(() => {
     axios
@@ -22,8 +25,11 @@ const RelatedCard = ({ item }) => {
         }
       })
       .then((results) => {
-        const url = results.data;
-        setImage(url);
+        const productStyle = results.data;
+        // console.log('productStyle', productStyle.results[0].photos[0].url);
+        console.log('productStyle', productStyle);
+        setStyle(productStyle);
+        setImgURL(productStyle.results[0].photos[0].thumbnail_url);
       })
       .catch((err) => {
         throw ('Error fetching product image');
@@ -33,29 +39,36 @@ const RelatedCard = ({ item }) => {
 
 
   return (
-    <Card
-      className='related-card'
-      sx={{ maxWidth: 250 }}
-      elevation={0}>
-      <StarBorderOutlinedIcon className='related-star'></StarBorderOutlinedIcon>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="100"
-          image={imageURL}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <React.Fragment>
+      {style &&
+        <Card
+          className='related-card'
+          sx={{ maxWidth: 250 }}
+          elevation={0}>
+          <StarBorderOutlinedIcon className='related-star'></StarBorderOutlinedIcon>
+          <CardActionArea>
+            {imgURL
+              ? <CardMedia
+                component="img"
+                height="100%"
+                image={imgURL}
+                alt="green iguana"
+              />
+              : <div className='related-image'>No image</div>
+            }
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      }
+    </React.Fragment>
   );
 };
 
