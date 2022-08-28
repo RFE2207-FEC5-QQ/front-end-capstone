38d98fea126277db26492ef1220117de6aa89931
Daniel Shin <daniel990402@gmail.com>
2022-08-27 20:29:07 -0400

    added server route and implemented description

--- client/src/components/overview/Description.jsx
+++ client/src/components/overview/Description.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Description = (props) => {
-  return(
-    <div className='overview-description'>
-      Description
-    </div>
-  )
+const Description = ({ product }) => {
+
+  const [info, setInfo] = useState({});
+
+  const getProductInfo = (id) => {
+    var options = {
+      method: 'get',
+      url: '/info',
+      params: {
+        id: id
+      }
+    }
+    axios(options)
+      .then(res => {
+        setInfo(res.data)
+      })
+  }
+
+  useEffect(() => {
+    getProductInfo(product.id)
+  }, [])
+
+  if (Object.keys(info).length) {
+    return(
+      <div className='overview-description'>
+        <div className='product-slogan'>
+          {product.slogan}
+        </div>
+        <div className='product-description'>
+          {product.description}
+        </div>
+        <ul className='product-features'>
+          {info.features.map(feat => {
+            return (
+              <li key={feat.feature}>
+                <div>{feat.feature} - {feat.value}</div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Description;
--- server/routes/details.js
+++ server/routes/details.js
@@ -20,6 +20,24 @@ router.get('/details', (req, res, next) => {
     })
 });
 
+router.get('/info', (req, res, next) => {
+  var id = req.query.id;
+  var options = {
+    method: 'get',
+    url: `${process.env.API_URL}products/${id}/`,
+    headers: {
+      'Authorization': process.env.GITHUB_AUTH
+    }
+  }
+  axios(options)
+    .then(result => {
+      res.status(200).send(result.data);
+    })
+    .catch(err => {
+      res.sendStatus(404);
+    })
+})
+
 router.get('/styles', (req, res, next) => {
   var id = req.query.id
   var options = {
