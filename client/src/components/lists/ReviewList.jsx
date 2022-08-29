312403b7a6224664ef1883dfac4c43f23ee6d749
Ryan Nelson <rnelson91295@gmail.com>
2022-08-29 14:21:44 -0400

    Refactored Reviews.jsx to store state of reviews to share with ReviewList.jsx, added ability to filter reviews

--- client/src/components/lists/ReviewList.jsx
+++ client/src/components/lists/ReviewList.jsx
@@ -1,147 +1,42 @@
 import React from 'react';
-import axios from 'axios';
 
 import {InputLabel, MenuItem, FormControl} from '@mui/material';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 import Review from '../cards/Review.jsx';
 
-class ReviewList extends React.Component {
+const ReviewList = ({reviews, sort, getReviews, handleSortChange, handleMoreReviews, ratingTheme, paletteMap}) => {
 
-  // Props:
-  // paletteMap
-  // ratingTheme
-  constructor(props) {
-    super(props);
-    // TODO: Add selector for number of reviews to display, page number
-    this.state = {
-      productId: 37311,
-      // DEBUG - Using sample review
-      reviews: [
-        {
-          'review_id': 1275440,
-          'rating': 5,
-          'summary': 'This is a summary...',
-          'recommend': true,
-          'response': null,
-          'body': 'Let me tell you about being a cat, it isn\'t always great! Consectetur voluptate sit cillum in occaecat sit. Commodo ipsum ad adipisicing adipisicing. Cupidatat non esse elit proident duis incididunt laborum. Laboris eiusmod in pariatur. Excepteur sint occaecat ullamco cillum ea minim deserunt. Officia labore id sunt consectetur amet eiusmod. Mollit do nisi ipsum aute. Laborum eiusmod occaecat sunt enim enim laborum.',
-          'date': '2022-07-15T00:00:00.000Z',
-          'reviewer_name': 'An Actual Real Life Cat',
-          'helpfulness': 2,
-          'photos': [
-            {
-              'id': 2455345,
-              'url': 'http://res.cloudinary.com/dm84tjpoq/image/upload/v1657918306/vw1hfv268xkgpyfr0i04.jpg'
-            },
-            {
-              'id': 1111111,
-              'url': 'https://i.imgur.com/EPHb3G6.jpeg'
-            },
-            {
-              'id': 1111112,
-              'url': 'https://i.imgur.com/EPHb3G6.jpeg'
-            },
-            {
-              'id': 1111113,
-              'url': 'https://i.imgur.com/EPHb3G6.jpeg'
-            }
-          ]
-        }
-      ],
-      sort: 'newest',
-      count: 2,
-      page: 1
-    };
-    this.handleSortChange = this.handleSortChange.bind(this);
-    this.handleMoreReviews = this.handleMoreReviews.bind(this);
-    this.getReviews = this.getReviews.bind(this);
-    this.postReview = this.postReview.bind(this);
-  }
+  console.log('reviews', reviews); // DEBUG
 
-  handleSortChange(e) {
-    // After state is set, use getReviews as a callback to get sorted list of reviews
-    this.setState({sort: e.target.value}, this.getReviews);
-  }
-
-  handleMoreReviews() {
-    // After state is set, use getReviews as a callback to get sorted list of reviews
-    this.setState({count: this.state.count + 2}, this.getReviews);
-  }
-
-  getReviews() {
-    axios.get('/reviews', {
-      params: {
-        productId: this.state.productId,
-        sort: this.state.sort,
-        count: this.state.count,
-        page: this.state.page
-      }
-    })
-      .then((success) => {
-        this.setState({reviews: success.data.results});
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  postReview() {
-    // DEBUG - Using test values
-    axios.post('/reviews', {
-      productId: 37311,
-      rating: 5,
-      summary: 'Just so great',
-      body: 'Seriously just the best piece of clothing in my wardrobe. Would give 6 stars if I could!',
-      recommend: true,
-      name: 'Ryan',
-      email: 'ryan@example.com',
-      photos: ['https://i.imgur.com/EPHb3G6.jpeg'],
-      characteristics: {}
-    })
-      .then((success) => {
-        console.log('POST success!'); // DEBUG
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  componentDidMount() {
-    // DEBUG - Uncomment to get reviews on mount
-    // this.getReviews();
-  }
-
-  render() {
-    console.log('reviews', this.state.reviews); // DEBUG
-    return (
-      <div className='review-list'>
-        <div className='review-list-top'>
-          {`${this.state.reviews.length} ${this.state.reviews.length === 1 ? 'review' : 'reviews'}, sorted by `}
-          <Select
-            variant="standard"
-            labelId="sort-select-label"
-            id="sort-select"
-            value={this.state.sort}
-            onChange={this.handleSortChange}
-            label="Sort"
-          >
-            <MenuItem value={'newest'}>newest</MenuItem>
-            <MenuItem value={'helpful'}>helpful</MenuItem>
-            <MenuItem value={'relevant'}>relevance</MenuItem>
-          </Select>
-        </div>
-        {
-          this.state.reviews.length === 0 ? 'Reviews Not Found' : this.state.reviews.map((review) => (
-            <Review review={review} getReviews={this.getReviews} ratingTheme={this.props.ratingTheme} paletteMap={this.props.paletteMap} key={review.review_id}/>
-          ))
-        }
-        <div className='review-buttons'>
-          <button onClick={this.handleMoreReviews}>More Reviews</button> <button>Add a Review +</button>
-        </div>
+  return (
+    <div className='review-list'>
+      <div className='review-list-top'>
+        {`${reviews.length} ${reviews.length === 1 ? 'review' : 'reviews'}, sorted by `}
+        <Select
+          variant="standard"
+          labelId="sort-select-label"
+          id="sort-select"
+          value={sort}
+          onChange={handleSortChange}
+          label="Sort"
+        >
+          <MenuItem value={'newest'}>newest</MenuItem>
+          <MenuItem value={'helpful'}>helpful</MenuItem>
+          <MenuItem value={'relevant'}>relevance</MenuItem>
+        </Select>
       </div>
-    );
-  }
+      {
+        reviews.length === 0 ? 'Reviews Not Found' : reviews.map((review) => (
+          <Review review={review} getReviews={getReviews} ratingTheme={ratingTheme} paletteMap={paletteMap} key={review.review_id}/>
+        ))
+      }
+      <div className='review-buttons'>
+        <button onClick={handleMoreReviews}>More Reviews</button> <button>Add a Review +</button>
+      </div>
+    </div>
+  );
 
-}
+};
 
 export default ReviewList;
--- client/src/components/views/Reviews.jsx
+++ client/src/components/views/Reviews.jsx
@@ -1,88 +1,216 @@
 import React from 'react';
+import axios from 'axios';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import ReviewList from '../lists/ReviewList.jsx';
 import ReviewMeta from '../cards/ReviewMeta.jsx';
 
-const Reviews = () => {
-
-  const characteristicChart = {
-    Size: {
-      1: 'A size too small',
-      2: '1/2 a size too small',
-      3: 'Perfect',
-      4: '1/2 a size too big',
-      5: 'A size too wide'
-    },
-    Width: {
-      1: 'Too narrow',
-      2: 'Slightly narrow',
-      3: 'Perfect',
-      4: 'Slightly wide',
-      5: 'Too wide'
-    },
-    Comfort: {
-      1: 'Uncomfortable',
-      2: 'Slightly uncomfortable',
-      3: 'Ok',
-      4: 'Comfortable',
-      5: 'Perfect'
-    },
-    Quality: {
-      1: 'Poor',
-      2: 'Below average',
-      3: 'What I expected',
-      4: 'Pretty great',
-      5: 'Perfect'
-    },
-    Length: {
-      1: 'Runs short',
-      2: 'Runs slightly short',
-      3: 'Perfect',
-      4: 'Runs slightly long',
-      5: 'Runs long'
-    },
-    Fit: {
-      1: 'Runs tight',
-      2: 'Runs slightly tight',
-      3: 'Perfect',
-      4: 'Runs slightly long',
-      5: 'Runs long'
-    }
-  };
-
-  const ratingTheme = createTheme({
-    palette: {
-      primary: {
-        main: '#333333',
-      },
-      neutral: {
-        main: '#dfcc97',
-      },
-      success: {
-        main: '#90ee90',
-      },
-    },
-  });
-
-  const paletteMap = {
-    '1': ['error', '#ff3333'],
-    '2': ['warning', '#ff9966'],
-    '3': ['neutral', '#dfcc97'],
-    '4': ['info', '#66cce6'],
-    '5': ['success', '#90ee90']
-  };
-
-  return (
-    <div className='reviews-view'>
-      <h2>{'Ratings & Reviews'}</h2>
-      <div className='reviews-panels'>
-        <ReviewMeta ratingTheme={ratingTheme} paletteMap={paletteMap} characteristicChart={characteristicChart}/>
-        <ReviewList ratingTheme={ratingTheme} paletteMap={paletteMap}/>
-      </div>
-    </div>
-  );
-
+const characteristicChart = {
+  Size: {
+    1: 'A size too small',
+    2: '1/2 a size too small',
+    3: 'Perfect',
+    4: '1/2 a size too big',
+    5: 'A size too wide'
+  },
+  Width: {
+    1: 'Too narrow',
+    2: 'Slightly narrow',
+    3: 'Perfect',
+    4: 'Slightly wide',
+    5: 'Too wide'
+  },
+  Comfort: {
+    1: 'Uncomfortable',
+    2: 'Slightly uncomfortable',
+    3: 'Ok',
+    4: 'Comfortable',
+    5: 'Perfect'
+  },
+  Quality: {
+    1: 'Poor',
+    2: 'Below average',
+    3: 'What I expected',
+    4: 'Pretty great',
+    5: 'Perfect'
+  },
+  Length: {
+    1: 'Runs short',
+    2: 'Runs slightly short',
+    3: 'Perfect',
+    4: 'Runs slightly long',
+    5: 'Runs long'
+  },
+  Fit: {
+    1: 'Runs tight',
+    2: 'Runs slightly tight',
+    3: 'Perfect',
+    4: 'Runs slightly long',
+    5: 'Runs long'
+  }
 };
+
+const ratingTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#333333',
+    },
+    neutral: {
+      main: '#dfcc97',
+    },
+    success: {
+      main: '#90ee90',
+    },
+  },
+});
+
+const paletteMap = {
+  '1': ['error', '#ff3333'],
+  '2': ['warning', '#ff9966'],
+  '3': ['neutral', '#dfcc97'],
+  '4': ['info', '#66cce6'],
+  '5': ['success', '#90ee90']
+};
+
+class Reviews extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // TODO: Add selector for number of reviews to display, page number
+    this.state = {
+      productId: 37311,
+      // DEBUG - Using sample review
+      reviews: [
+        {
+          'review_id': 1275440,
+          'rating': 5,
+          'summary': 'This is a summary...',
+          'recommend': true,
+          'response': null,
+          'body': 'Let me tell you about being a cat, it isn\'t always great! Consectetur voluptate sit cillum in occaecat sit. Commodo ipsum ad adipisicing adipisicing. Cupidatat non esse elit proident duis incididunt laborum. Laboris eiusmod in pariatur. Excepteur sint occaecat ullamco cillum ea minim deserunt. Officia labore id sunt consectetur amet eiusmod. Mollit do nisi ipsum aute. Laborum eiusmod occaecat sunt enim enim laborum.',
+          'date': '2022-07-15T00:00:00.000Z',
+          'reviewer_name': 'An Actual Real Life Cat',
+          'helpfulness': 2,
+          'photos': [
+            {
+              'id': 2455345,
+              'url': 'http://res.cloudinary.com/dm84tjpoq/image/upload/v1657918306/vw1hfv268xkgpyfr0i04.jpg'
+            },
+            {
+              'id': 1111111,
+              'url': 'https://i.imgur.com/EPHb3G6.jpeg'
+            },
+            {
+              'id': 1111112,
+              'url': 'https://i.imgur.com/EPHb3G6.jpeg'
+            },
+            {
+              'id': 1111113,
+              'url': 'https://i.imgur.com/EPHb3G6.jpeg'
+            }
+          ]
+        }
+      ],
+      sort: 'relevant',
+      count: 2,
+      page: 1,
+      filter: {}
+    };
+    this.handleSortChange = this.handleSortChange.bind(this);
+    this.handleMoreReviews = this.handleMoreReviews.bind(this);
+    this.getReviews = this.getReviews.bind(this);
+    this.postReview = this.postReview.bind(this);
+  }
+
+  handleSortChange(e) {
+    // After state is set, use getReviews as a callback to get sorted list of reviews
+    this.setState({sort: e.target.value}, this.getReviews);
+  }
+
+  handleMoreReviews() {
+    // After state is set, use getReviews as a callback to get sorted list of reviews
+    this.setState({count: this.state.count + 2}, this.getReviews);
+  }
+
+  setFilter(filter) {
+    // After state is set, use getReviews as a callback to get sorted list of reviews
+    this.setState({filter}, this.getReviews);
+  }
+
+  getReviews() {
+    return axios.get('/reviews', {
+      params: {
+        productId: this.state.productId,
+        sort: this.state.sort,
+        count: this.state.count,
+        page: this.state.page
+      }
+    })
+      .then((success) => {
+        let reviews = success.data.results;
+        let filterObj = this.state.filter;
+        if (filterObj) {
+          let filteredReviews = [];
+          for (let review of reviews) {
+            let match = true;
+            for (let key in filterObj) {
+              if (review[key] !== filterObj[key]) {
+                match = false;
+                break;
+              }
+            }
+            if (match) {
+              filteredReviews.push(review);
+            }
+          }
+          reviews = filteredReviews;
+        }
+        this.setState({reviews});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  postReview() {
+    // DEBUG - Using test values
+    axios.post('/reviews', {
+      productId: 37311,
+      rating: 5,
+      summary: 'Just so great',
+      body: 'Seriously just the best piece of clothing in my wardrobe. Would give 6 stars if I could!',
+      recommend: true,
+      name: 'Ryan',
+      email: 'ryan@example.com',
+      photos: ['https://i.imgur.com/EPHb3G6.jpeg'],
+      characteristics: {}
+    })
+      .then((success) => {
+        console.log('POST success!'); // DEBUG
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    // DEBUG - Uncomment to get reviews on mount
+    this.getReviews();
+  }
+
+
+  render() {
+    return (
+      <div className='reviews-view'>
+        <h2>{'Ratings & Reviews'}</h2>
+        <div className='reviews-panels'>
+          <ReviewMeta filterbyRating={(ratingStars) => this.setFilter({rating: parseInt(ratingStars)})} paletteMap={paletteMap} characteristicChart={characteristicChart}/>
+          <ReviewList reviews={this.state.reviews} sort={this.state.sort} getReviews={this.getReviews} handleSortChange={this.handleSortChange} handleMoreReviews={this.handleMoreReviews} ratingTheme={ratingTheme} paletteMap={paletteMap}/>
+        </div>
+      </div>
+    );
+  }
+
+}
 
 export default Reviews;
