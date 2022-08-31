bce4c0d587f8223ae07677c732d574e4f1e1e4aa
Ryan Nelson <rnelson91295@gmail.com>
2022-08-30 21:26:42 -0400

    Input validation on reviews form fields, required before submitting

--- client/src/components/forms/ReviewForm.jsx
+++ client/src/components/forms/ReviewForm.jsx
@@ -12,34 +12,49 @@ export default class ReviewForm extends React.Component {
   // props.characteristicChart - Mapping of characteristic types to their value descriptions
   // props.productId - Currently selected product
   // props.paletteMap - Colors for the star ratings
+  // props.setErrorMessage - Sets an error message to display in parent component
   constructor(props) {
     super(props);
     // TODO: Ensure someone can't submit the form without all required values filled out
     this.state = {
+      errorMessage: null,
       rating: null, // 1 - 5
-      recommend: true, // Recommendation will be captured via a radio button array of “Yes” and “No”.
+      ratingValid: false,
+      recommend: null, // Recommendation will be captured via a radio button array of “Yes” and “No”.
       // Default radio button behavior will apply.
+      recommendValid: false,
       characteristics: {},
+      characteristicsValid: function(metaCharacteristics) {
+        let output = {};
+        for (let key in metaCharacteristics) {
+          output[key] = false;
+        }
+        return output;
+      }(this.props.metaCharacteristics),
       summary: '', // A text input allowing up to 60 characters.
       // Placeholder text should read: “Example: Best purchase ever!”
       // Optional
+      summaryValid: true,
       body: '', // A text input allowing up to 1000 characters.
       // Placeholder text should read: “Why did you like the product or not?”.
       // The review must be over 50 characters long in order to be submitted. If the user tries to submit a review shorter than 50 characters, then the submission should fail in the same manner as it would for a blank mandatory field.
+      bodyValid: false,
       photos: [], // Up to 5 photos allowed
       name: '', // A text input allowing up to 60 characters for the user’s display name.
       // Placeholder text should read: “Example: jackson11!”.
       // Below this field, the text “For privacy reasons, do not use your full name or email address” will appear.
+      nameValid: false,
       email: '', // A text input allowing up to 60 characters.
       // Placeholder text should read: “Example: jackson11@email.com”.
       // Below this field, the text “For authentication reasons, you will not be emailed” will appear.
+      emailValid: false
     };
     this.postReview = this.postReview.bind(this);
     this.submitForm = this.submitForm.bind(this);
   }
 
   postReview() {
-    return axios.post('/reviews', {
+    axios.post('/reviews', {
       productId: this.props.productId,
       rating: this.state.rating,
       summary: this.state.summary,
@@ -51,21 +66,21 @@ export default class ReviewForm extends React.Component {
       characteristics: this.state.characteristics
     })
       .then((success) => {
-        console.log('POST success!'); // DEBUG
+        this.props.postSubmit();
       })
       .catch((error) => {
         console.log(error);
+        this.setState({errorMessage: `There was an error submitting the form: ${error.message}`});
       });
   }
 
   submitForm(e) {
     e.preventDefault();
     this.postReview();
-    this.props.postSubmit();
   }
 
   render() {
-    // console.log(this.state);
+    console.log(this.state.characteristicsValid);
     return (
       <form className='review-form-content' onSubmit={this.submitForm}>
         <div className='review-form-rating'>
@@ -77,7 +92,11 @@ export default class ReviewForm extends React.Component {
             name="rating"
             value={this.state.rating}
             onChange={(e) =>
-              this.setState({rating: parseInt(e.target.value)})
+              this.setState({rating: parseInt(e.target.value)}, () => {
+                if (!this.state.ratingValid) {
+                  this.setState({ratingValid: true});
+                }
+              })
             }
           />
         </div>
@@ -88,9 +107,13 @@ export default class ReviewForm extends React.Component {
               type='radio'
               name='recommend-true'
               value='true'
-              checked={this.state.recommend}
+              checked={this.state.recommend === true}
               onChange={(e) =>
-                this.setState({recommend: true})
+                this.setState({recommend: true}, () => {
+                  if (!this.state.recommendValid) {
+                    this.setState({recommendValid: true});
+                  }
+                })
               }
             />
             Yes
@@ -100,9 +123,13 @@ export default class ReviewForm extends React.Component {
               type='radio'
               name='recommend-false'
               value='false'
-              checked={!this.state.recommend}
+              checked={this.state.recommend === false}
               onChange={(e) =>
-                this.setState({recommend: false})
+                this.setState({recommend: false}, () => {
+                  if (!this.state.recommendValid) {
+                    this.setState({recommendValid: true});
+                  }
+                })
               }
             />
             No
@@ -125,6 +152,11 @@ export default class ReviewForm extends React.Component {
                         let characteristics = this.state.characteristics;
                         characteristics[this.props.metaCharacteristics[characteristic].id] = parseInt(e.target.value);
                         this.setState({characteristics});
+                        if (!this.state.characteristicsValid[characteristic]) {
+                          let characteristicsValid = this.state.characteristicsValid;
+                          characteristicsValid[characteristic] = true;
+                          this.setState({characteristicsValid});
+                        }
                       }}
                     />
                     {this.props.characteristicChart[characteristic][key]}
@@ -143,9 +175,13 @@ export default class ReviewForm extends React.Component {
               placeholder='Example: Best purchase ever!'
               value={this.state.summary}
               onChange={(e) =>
-                this.setState({summary: e.target.value})
+                this.setState({summary: e.target.value}, () => {
+                  let isValid = this.state.summary.length <= 60;
+                  this.setState({summaryValid: isValid});
+                })
               }
             />
+            {'Characters left: ' + (60 - this.state.summary.length).toString()}
           </label>
         </div>
         <div className='review-form-body'>
@@ -156,9 +192,13 @@ export default class ReviewForm extends React.Component {
               placeholder='Why did you like the product or not?'
               value={this.state.body}
               onChange={(e) =>
-                this.setState({body: e.target.value})
+                this.setState({body: e.target.value}, () => {
+                  let isValid = 50 <= this.state.body.length && this.state.body.length <= 1000;
+                  this.setState({bodyValid: isValid});
+                })
               }
             />
+            {'Characters left: ' + (1000 - this.state.body.length).toString()}
           </label>
         </div>
         <div className='review-form-photos'>
@@ -173,9 +213,14 @@ export default class ReviewForm extends React.Component {
               placeholder='Example: jackson11!'
               value={this.state.name}
               onChange={(e) =>
-                this.setState({name: e.target.value})
+                this.setState({name: e.target.value}, () => {
+                  let isValid = 0 < this.state.name.length && this.state.name.length <= 60;
+                  this.setState({nameValid: isValid});
+                })
               }
             />
+            {'Characters left: ' + (60 - this.state.name.length).toString()}
+            {'For privacy reasons, do not use your full name or email address'}
           </label>
         </div>
         <div className='review-form-email'>
@@ -187,12 +232,40 @@ export default class ReviewForm extends React.Component {
               placeholder='Example: jackson11@email.com'
               value={this.state.email}
               onChange={(e) =>
-                this.setState({email: e.target.value})
+                this.setState({email: e.target.value}, () => {
+                  let isValid = 0 < this.state.email.length <= 60;
+                  this.setState({emailValid: isValid});
+                })
               }
             />
+            {'Characters left: ' + (60 - this.state.email.length).toString()}
+            {'For authentication reasons, you will not be emailed'}
           </label>
         </div>
-        <input type='submit' value='Submit' />
+        <input
+          type='submit'
+          value='Submit'
+          disabled={
+            !(this.state.ratingValid &&
+              this.state.recommendValid &&
+              this.state.summaryValid &&
+              this.state.bodyValid &&
+              this.state.nameValid &&
+              this.state.emailValid &&
+              function(characteristicsValid) {
+                for (let characteristic in characteristicsValid) {
+                  if (!characteristicsValid[characteristic]) {
+                    return false;
+                  }
+                }
+                return true;
+              }(this.state.characteristicsValid)
+            )
+          }
+        />
+        <div className='review-form-error'>
+          {this.state.errorMessage}
+        </div>
       </form>
     );
   }
