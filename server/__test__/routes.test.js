63038b529ae9ca8f9d010d02b5c4d2d2a0186415
Ryan Nelson <rnelson91295@gmail.com>
2022-08-31 15:28:47 -0400

    Using async/await on server tests

--- server/__test__/routes.test.js
+++ server/__test__/routes.test.js
@@ -2,13 +2,21 @@ const request = require('supertest');
 const app = require('../app.js');
 
 
-describe('Routes testing', () => {
+describe('\'/reviews\' routes', () => {
 
-  test('Expect 2 reviews to be fetched from server on GET request to reviews endpoint', () => {
-    request(app).get('/reviews?productId=37311&sort=newest&count=2&page=1')
+  let reviewCount = 2;
+  test(`Expect ${reviewCount} reviews to be fetched from server`, async () => {
+    await request(app)
+      .get('/reviews')
+      .query({
+        productId: '37311',
+        sort: 'newest',
+        count: reviewCount,
+        page: 1
+      })
       .then((success) => {
         let reviews = success.body.results;
-        expect(reviews.length).toBe(2);
+        expect(reviews.length).toBe(reviewCount);
       });
   });
 
