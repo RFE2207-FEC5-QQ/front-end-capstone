dd5e1fd9bafa083c0e6cd9c001770db21d1dd778
Ryan Nelson <rnelson91295@gmail.com>
2022-08-31 15:21:04 -0400

    Updated linting rules to allow for ES6 / Node syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: 'hackreactor',
   rules: {
+    "env": {
+      "node": true,
+      "es6": true
+    }
     // Rules here will override the 'hackreactor' configuration
     // http://eslint.org/docs/rules/
   }
