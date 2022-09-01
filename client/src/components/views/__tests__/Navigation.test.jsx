6b685441cb45165a9a5bfb866bb6cbc841a73e02
ytang <107004276+yota88@users.noreply.github.com>
2022-08-31 21:01:28 -0400

    Completed first unit test for toggle dark mode button.

--- client/src/components/views/__tests__/Navigation.test.jsx
+++ client/src/components/views/__tests__/Navigation.test.jsx
@@ -1,2 +1,38 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import Navigation from '../Navigation.jsx';
+
+
+describe('Testing dark theme toggle button', () => {
+
+  it('Should show light icon and not dark icon when dark mode is off', () => {
+    const darkOff = {darkMode: false, punkMode: false, psychMode: false};
+    const toggleThemes = {
+      toggleDark: null,
+      toggleGod: null,
+      toggleLudi: null,
+      togglePsych: null,
+      togglePunk: null
+    };
+    render(<Navigation modes={darkOff} toggleTheme={toggleThemes}/>);
+    expect(screen.queryByLabelText('light-icon')).toBeInTheDocument();
+    expect(screen.queryByLabelText('dark-icon')).not.toBeInTheDocument();
+  });
+
+  it('Should show dark icon and not light icon when dark mode is on', () => {
+    const darkOn = {darkMode: true, punkMode: false, psychMode: false};
+    const toggleThemes = {
+      toggleDark: null,
+      toggleGod: null,
+      toggleLudi: null,
+      togglePsych: null,
+      togglePunk: null
+    };
+    render(<Navigation modes={darkOn} toggleTheme={toggleThemes}/>);
+    expect(screen.queryByLabelText('light-icon')).not.toBeInTheDocument();
+    expect(screen.queryByLabelText('dark-icon')).toBeInTheDocument();
+  });
+
+});
