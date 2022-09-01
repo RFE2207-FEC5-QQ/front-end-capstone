c1f71f0afaf6734805d5dc62914b4b495db5ff03
ytang <107004276+yota88@users.noreply.github.com>
2022-08-31 20:30:28 -0400

    Updated navigation component to enable multiple themes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import Navigation from './components/views/Navigation.jsx';
 import Overview from './components/views/Overview.jsx';
 import RelatedProducts from './components/views/RelatedProducts.jsx';
@@ -10,24 +11,75 @@ class App extends React.Component {
     super(props);
     this.state = {
       productId: 0,
-      darkMode: false,
+      darkMode: JSON.parse(window.localStorage.getItem('dark')) ?? false,
+      punkMode: false,
+      psychMode: false,
     };
-    this.toggleColorMode = this.toggleColorMode.bind(this);
+    this.darkMode = this.darkMode.bind(this);
+    this.punkMode = this.punkMode.bind(this);
+    this.psychMode = this.psychMode.bind(this);
   }
 
-  toggleColorMode() {
+  darkMode() {
     this.setState({
       productId: this.state.productId,
       darkMode: !this.state.darkMode,
+      punkMode: this.state.punkMode,
+      psychMode: this.state.psychMode,
     });
   }
 
+  punkMode() {
+    this.setState({
+      productId: this.state.productId,
+      darkMode: this.state.darkMode,
+      punkMode: !this.state.punkMode,
+      psychMode: this.state.psychMode,
+    });
+  }
+
+  psychMode() {
+    this.setState({
+      productId: this.state.productId,
+      darkMode: this.state.darkMode,
+      punkMode: this.state.punkMode,
+      psychMode: !this.state.psychMode,
+    });
+  }
+
+  componentDidMount() {
+    if (this.state.darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.darkMode !== prevState.darkMode) {
+      window.localStorage.setItem('dark', JSON.stringify(this.state.darkMode));
+      document.body.classList.toggle('dark-mode');
+    }
+  }
+
   render() {
+    const themeTogglers = {
+      toggleDark: this.darkMode,
+      togglePunk: this.punkMode,
+      togglePsych: this.psychMode,
+    };
+
+    const modes = {
+      darkMode: this.state.darkMode,
+      punkMode: this.state.punkMode,
+      psychMode: this.state.psychMode,
+    };
+
     return (
       <React.Fragment>
-        <Navigation onChange={this.toggleColorMode}/>
+        <Navigation modes={modes} toggleTheme={themeTogglers}/>
         <Overview/>
-        <RelatedProducts/>
+        <RelatedProducts modes={modes}/>
         <QuestionsAnswers/>
         <Reviews/>
       </React.Fragment>
