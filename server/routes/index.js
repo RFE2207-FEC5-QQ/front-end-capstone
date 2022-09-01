c3c3c5d5fd422e78120fb53f94ce8d27d3ced0a3
Ryan Nelson <rnelson91295@gmail.com>
2022-09-01 19:16:24 -0400

    Removed placeholder handlers

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   details: require('./details.js'),
-  related: require('./related.js'),
   reviews: require('./reviews.js'),
   cart: require('./cart.js'),
   interactions: require('./interactions.js'),
