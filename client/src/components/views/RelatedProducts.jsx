e0f16be8bcf730074197bcc63530c7e28f5fc77c
Ryan Nelson <rnelson91295@gmail.com>
2022-09-03 22:09:44 -0400

    testing revert

--- client/src/components/views/RelatedProducts.jsx
+++ client/src/components/views/RelatedProducts.jsx
@@ -1,45 +1,9 @@
-import React, { useState, useEffect } from 'react'; // React module is imported if you choose to convert to class component, remove the import if not
-import axios from 'axios';
-import { Swiper, SwiperSlide } from 'swiper/react';
-// import RelatedCard from './RelatedCard.jsx';
-import Carousel from 'react-material-ui-carousel';
-import { Paper, Button } from '@mui/material';
+import React from 'react'; // React module is imported if you choose to convert to class component, remove the import if not
 
 const RelatedProducts = () => {
-  // Use placeholder productId passed down from global state for now.
-  const [related, setRelated] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get('/related', {
-        params: {
-          // productId
-          productId: 1,
-        }
-      })
-      .then((results) => {
-        setRelated(results);
-      })
-      .catch((err) => {
-        throw ('Error fetching related products');
-      });
-  // }, [productId]);
-  }, [1]);
-
-
-
-  // return (
-  //   <div className='view-related'>
-  //   </div>
-  // );
-
   return (
-    <Carousel>
-      {/* <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/> */}
-    </Carousel>
+    <div className='view-related'>
+    </div>
   );
 };
 
