d8fa2d53d813245c4cb0e794d4feaea7435e38e3
Ryan Nelson <rnelson91295@gmail.com>
2022-09-03 21:40:59 -0400

    Updated routes

--- server/routes/related.js
+++ server/routes/related.js
@@ -50,48 +50,4 @@ router.get('/styles', (req, res, next) => {
     });
 });
 
-// Temporary. Will use Daniel's router in final version.
-router.get('/details', (req, res, next) => {
-  var id = req.query.productId;
-  console.log('id', id, 'req query', req.query);
-  var options = {
-    method: 'get',
-    url: `${process.env.API_URL}products/${id}`,
-    headers: {
-      'Authorization': process.env.GITHUB_AUTH
-    }
-  };
-  axios(options)
-    .then(result => {
-      console.log('detail result', result);
-      res.status(200).send(result.data);
-    })
-    .catch((err) => {
-      res.sendStatus(404);
-    });
-});
-
-// Also temporary. Will use Ryan's route when finalized.
-router.get('/reviews/meta', (req, res, next) => {
-  if (!req.query.productId) {
-    res.sendStatus(404);
-    return;
-  }
-  axios.get(process.env.API_URL + 'reviews/meta',
-    {
-      params: {
-        'product_id': req.query.productId
-      },
-      headers: { 'Authorization': process.env.GITHUB_AUTH }
-    }
-  )
-    .then((success) => {
-      res.status(200).send(success.data);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.sendStatus(404);
-    });
-});
-
 module.exports = router;
