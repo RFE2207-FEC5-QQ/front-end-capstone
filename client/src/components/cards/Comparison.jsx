f3d3ea65b544fb292619b66b11716160f3c68417
ytang <107004276+yota88@users.noreply.github.com>
2022-09-04 07:45:18 -0400

    Clean branch to merge with main. Updated to latest funtional state with outfit and relate cards.

---
+++ client/src/components/cards/Comparison.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from 'react';
+import Popup from 'reactjs-popup';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+
+const Comparison = ({ mainProduct, currProduct }) => {
+  const [open, setOpen] = useState(false);
+  const [features, setFeatures] = useState([]);
+  const [mainValues, setMainValues] = useState([]);
+  const [currValues, setCurrValues] = useState([]);
+  const [combinedVal, setCombinedVal] = useState([]);
+
+  useEffect(() => {
+    const allFeatures = new Set();
+    const currFeatures = [];
+    const mainFeatures = [];
+    const currValues = [];
+    const mainValues = [];
+    const combined = [];
+
+    mainProduct.features.forEach((featureObj) => {
+      allFeatures.add(featureObj.feature);
+      mainFeatures.push(featureObj.feature);
+    });
+    currProduct.features.forEach((featureObj) => {
+      allFeatures.add(featureObj.feature);
+      currFeatures.push(featureObj.feature);
+    });
+
+    const arrFeatures = Array.from(allFeatures);
+    setFeatures(arrFeatures);
+
+    arrFeatures.forEach((feature) => {
+      const mainIndex = mainFeatures.indexOf(feature);
+      const currIndex = currFeatures.indexOf(feature);
+      if (mainIndex !== -1) {
+        if (mainProduct.features[mainIndex].value === null) {
+          mainValues.push('N/A');
+        } else {
+          mainValues.push(mainProduct.features[mainIndex].value);
+        }
+      } else {
+        mainValues.push('N/A');
+      }
+      if (currIndex !== -1) {
+        if (currProduct.features[currIndex].value === null) {
+          currValues.push('N/A');
+        } else {
+          currValues.push(currProduct.features[currIndex].value);
+        }
+      } else {
+        currValues.push('N/A');
+      }
+    });
+
+    // console.log('arrfeatures', arrFeatures);
+    // console.log('currFeatures', currFeatures);
+    // console.log('currValues', currValues);
+    // console.log('mainFeatures', mainFeatures);
+    // console.log('mainValues', mainValues);
+
+    const mapFeatures = arrFeatures.map((value) => <span className='comp-values'>{value}</span>);
+    const mapCurrValues = currValues.map((value) => <span className='comp-values'>{value}</span>);
+    const mapMainValues = mainValues.map((value) => <span className='comp-values'>{value}</span>);
+
+
+
+    for (let i = 0; i < arrFeatures.length; i++) {
+      combined.push(<div>{mapCurrValues[i]} {mapFeatures[i]} {mapMainValues[i]}</div>);
+    }
+    console.log(combined);
+    setCombinedVal(combined);
+    // setMainValues(mainValues);
+    // setCurrValues(currValues);
+
+  }, [mainProduct, currProduct]);
+
+
+  const handleClose = () => setOpen(false);
+
+  return (
+    <React.Fragment>
+      <Popup
+        trigger={<StarBorderOutlinedIcon
+          className='modal-button'
+          onClick={() => setOpen(open => !open)}
+        />
+        }
+        open={open}
+        onClose={handleClose}
+        closeOnDocumentClick
+        position='bottom right'>
+        <div className="comparison-modal theme">
+          <div className='comparison-header-container'>
+            <div className='comparison-title'>COMPARING</div>
+            <div className='comparison-contents product-name'>
+              <div>{currProduct.name}</div>
+              <div></div>
+              <div>{mainProduct.name}</div>
+            </div>
+          </div>
+          <div className='comparison-content'>
+            {combinedVal}
+            {/* <div className='product-current'>
+              {currValues.map((value) => <div>{value}</div>)}
+            </div>
+            <div className='product-comparator'>
+              {features.map((feature) => <div>{feature}</div>)}
+            </div>
+            <div className='product-main'>
+              {mainValues.map((value) => <div>{value}</div>)}
+            </div> */}
+          </div>
+        </div>
+      </Popup>
+    </React.Fragment>
+  );
+};
+
+export default Comparison;
--- client/src/components/views/RelatedProducts.jsx
+++ client/src/components/views/RelatedProducts.jsx
@@ -1,8 +1,75 @@
-import React from 'react'; // React module is imported if you choose to convert to class component, remove the import if not
+import React, { useState, useEffect } from 'react'; // React module is imported if you choose to convert to class component, remove the import if not
+import axios from 'axios';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import RelatedCard from '../cards/RelatedCard.jsx';
+import { Paper, Button, Box } from '@mui/material';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
-const RelatedProducts = () => {
+const responsive = {
+  // superLargeDesktop: {
+  //   // the naming can be any, depends on you.
+  //   breakpoint: { max: 4000, min: 3000 },
+  //   items: 5
+  // },
+  desktop: {
+    breakpoint: { max: 3000, min: 464 },
+    items: 3
+  },
+  // tablet: {
+  //   breakpoint: { max: 1024, min: 464 },
+  //   items: 3
+  // },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+
+const RelatedProducts = ({ onClick, productId, product, modes }) => {
+  const [relatedList, setRelatedList] = useState(null);
+  const modalIcon = StarBorderOutlinedIcon;
+  const { psychMode } = modes;
+
+  useEffect(() => {
+    axios
+      .get('/related', {
+        params: {
+          // Placeholder productId for now.
+          productId,
+        }
+      })
+      .then((results) => {
+        const relatedIds = [...new Set(results.data)];
+        if (relatedIds.includes(productId)) {
+          relatedIds.splice(relatedIds.indexOf(productId), 1);
+        }
+        // console.log('relatedIds', relatedIds);
+        setRelatedList(relatedIds);
+      })
+      .catch((err) => {
+        throw ('Error fetching related products', productId);
+      });
+  }, [productId, product]);
+
   return (
-    <div className='view-related'>
+    <div className='related-products'>
+      <div className='related-header'>Related Products</div>
+      {relatedList
+        ? <Carousel className='carousel' responsive={responsive}>
+          {relatedList.map((relProduct, i) =>
+            <RelatedCard
+              onClick={onClick}
+              psychMode={psychMode}
+              key={i}
+              item={relProduct}
+              mainProduct={product}
+              modal='related'/>)}
+        </Carousel>
+        : null
+      }
     </div>
   );
 };
