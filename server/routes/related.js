844f3f986347c9b686b157bbdbd75b6add0c2ea5
Ryan Nelson <rnelson91295@gmail.com>
2022-09-03 22:21:47 -0400

    Cleaning dev

--- server/routes/related.js
+++ server/routes/related.js
@@ -30,24 +30,4 @@ router.post('/related', (req, res, next) => {
 
 });
 
-// Temporary. Will use Daniel's router in final version.
-router.get('/styles', (req, res, next) => {
-  var id = req.query.productId;
-  var options = {
-    method: 'get',
-    url: `${process.env.API_URL}products/${id}/styles`,
-    headers: {
-      'Authorization': process.env.GITHUB_AUTH
-    }
-  };
-  axios(options)
-    .then(result => {
-      console.log('styles data', result.data['product_id'], result.data.results[0].photos[0]);
-      res.status(200).send(result.data);
-    })
-    .catch(err => {
-      res.sendStatus(404);
-    });
-});
-
 module.exports = router;
