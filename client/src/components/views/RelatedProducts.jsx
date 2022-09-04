becab8e11c058c10c84f982816c48164f995e149
Ryan Nelson <rnelson91295@gmail.com>
2022-09-03 21:49:12 -0400

    Updated dev with main

--- client/src/components/views/RelatedProducts.jsx
+++ client/src/components/views/RelatedProducts.jsx
@@ -1,9 +1,45 @@
-import React from 'react'; // React module is imported if you choose to convert to class component, remove the import if not
+import React, { useState, useEffect } from 'react'; // React module is imported if you choose to convert to class component, remove the import if not
+import axios from 'axios';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import RelatedCard from './RelatedCard.jsx';
+import Carousel from 'react-material-ui-carousel';
+import { Paper, Button } from '@mui/material';
 
 const RelatedProducts = () => {
+  // Use placeholder productId passed down from global state for now.
+  const [related, setRelated] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('/related', {
+        params: {
+          // productId
+          productId: 1,
+        }
+      })
+      .then((results) => {
+        setRelated(results);
+      })
+      .catch((err) => {
+        throw ('Error fetching related products');
+      });
+  // }, [productId]);
+  }, [1]);
+
+
+
+  // return (
+  //   <div className='view-related'>
+  //   </div>
+  // );
+
   return (
-    <div className='view-related'>
-    </div>
+    <Carousel>
+      {/* <RelatedCard/>
+      <RelatedCard/>
+      <RelatedCard/>
+      <RelatedCard/> */}
+    </Carousel>
   );
 };
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,63 +1,6 @@
 import React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { createRoot } from 'react-dom/client';
-
-import Navigation from './components/views/Navigation.jsx';
-import Overview from './components/views/Overview.jsx';
-import RelatedProducts from './components/views/RelatedProducts.jsx';
-import QuestionsAnswers from './components/views/QuestionsAnswers.jsx';
-import Reviews from './components/views/Reviews.jsx';
-
-// Sprint 1 exercise:
-// This comment is here for testing out creation of dev branch and pushing to central repo.
-// A pull request will be made on dev branch.
+import App from './App.jsx';
 
 const root = createRoot(document.getElementById('root'));
-
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  }
-});
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  }
-});
-
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      productId: 37311 // TODO: Change to currently viewed product ID using context
-    };
-  }
-
-  render() {
-    return (
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline/>
-        <Navigation/>
-        <Overview/>
-        <RelatedProducts/>
-        <QuestionsAnswers/>
-        <Reviews productId={this.state.productId}/>
-      </ThemeProvider>
-    );
-  }
-}
-
 root.render(<App />);
