b24bfb21a5797da36123623aeb2bceae1648dba5
Daniel Shin <daniel990402@gmail.com>
2022-09-06 12:40:47 -0400

    implemented add to cart function

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -4,11 +4,30 @@ const axios = require('axios');
 const router = express.Router();
 
 router.get('/cart', (req, res, next) => {
-
+  axios.get(`${process.env.API_URL}cart`)
+    .then((result) => {
+      res.status(200).send(result.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 });
 
 router.post('/cart', (req, res, next) => {
-
+  axios.post(`${process.env.API_URL}cart`,
+    req.body,
+    {
+      headers: {
+        'Authorization': process.env.GITHUB_AUTH
+      }
+    }
+  )
+  .then(() => {
+    res.sendStatus(201);
+  })
+  .catch(() => {
+    res.sendStatus(422);
+  })
 });
 
 module.exports = router;
