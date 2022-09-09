bda302c0c2c5df5dc9dfc83e653b06c171a25a0c
ytang <107004276+yota88@users.noreply.github.com>
2022-09-09 08:43:41 -0400

    Added actual contact link to hackreactor dot com

--- client/src/components/views/Contact.jsx
+++ client/src/components/views/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       <div>|</div>
       <div>2022 Astelle US</div>
       <div>|</div>
-      <div>hackreactor.com</div>
+      <a className='hack-reactor' href='https://www.hackreactor.com'>hackreactor.com</a>
     </div>
   );
 };
