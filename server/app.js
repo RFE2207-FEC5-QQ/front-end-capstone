a80452d575692a2af9af52eaca06a3d4714d00a4
Ryan Nelson <rnelson91295@gmail.com>
2022-09-11 16:38:18 -0400

    serving compressed resources from server

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routers = require('./routes');
+var compression = require('compression');
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(express.json());
 for (let router of Object.values(routers)) {
